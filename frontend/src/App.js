f1aab972a1db7fc1c9c51613e346e55ec267e7ad
naopandao <nao_yoshida@yunixy.co.jp>
2022-08-16 21:05:41 +0900

    YX-221 店舗一覧、商品一覧、注文ページをReactRouterで設定

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
+// components
+import { Restaurants } from './containers/Restaurants.jsx';
+import { Foods } from './containers/Foods.jsx';
+import { Orders } from './containers/Orders.jsx';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-        Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Router>
+      <Switch>
+    
+        <Route
+          exact
+          path="/restaurants">
+          <Restaurants />
+        </Route>
+      
+        <Route
+          exact
+          path="/foods"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Foods />
+        </Route>
+       
+        <Route
+          exact
+          path="/orders">
+          <Orders />
+        </Route>
+        <Route
+        exact
+        path="/restaurants/:restaurantsId/foods"
+        render={({ match }) =>
+          <Foods
+            match={match}
+          />
+      }
+      />
+      </Switch>
+    </Router>
   );
 }
 
